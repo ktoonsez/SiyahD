ff5502312e440b05cc19e8660171382c31ff4f2c
Richard Kennedy <richard@rsk.demon.co.uk>
2011-07-15 11:41:31 +0100

    sched: Reorder root_domain to remove 64 bit alignment padding
    Reorder root_domain to remove 8 bytes of alignment padding on 64 bit
    builds, this shrinks the size from 1736 to 1728 bytes, therefore using
    one fewer cachelines.
    
    Signed-off-by: Richard Kennedy <richard@rsk.demon.co.uk>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1310726492.1977.5.camel@castor.rsk
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -428,6 +428,7 @@ struct rt_rq {
  */
 struct root_domain {
 	atomic_t refcount;
+	atomic_t rto_count;
 	struct rcu_head rcu;
 	cpumask_var_t span;
 	cpumask_var_t online;
@@ -437,7 +438,6 @@ struct root_domain {
 	 * one runnable RT task.
 	 */
 	cpumask_var_t rto_mask;
-	atomic_t rto_count;
 	struct cpupri cpupri;
 };
 
