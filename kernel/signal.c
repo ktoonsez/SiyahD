06cbc0cc6eb5d01813cdd79842b9ca9d0b7b486f
Oleg Nesterov <oleg@redhat.com>
2011-11-22 21:37:41 +0100

    tracing: send_sigqueue() needs trace_signal_generate() too
    Add trace_signal_generate() into send_sigqueue().
    
    send_sigqueue() is very similar to __send_signal(), just it uses
    the preallocated info. It should do the same wrt tracing.
    
    Reported-by: Seiji Aguchi <saguchi@redhat.com>
    Reviewed-by: Seiji Aguchi <seiji.aguchi@hds.com>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    (cherry picked from commit 163566f60bfe6a8176650155e2d98649b0dfabf8)

--- kernel/signal.c
+++ kernel/signal.c
@@ -1510,7 +1510,7 @@ int send_sigqueue(struct sigqueue *q, struct task_struct *t, int group)
 	int sig = q->info.si_signo;
 	struct sigpending *pending;
 	unsigned long flags;
-	int ret;
+	int ret, result;
 
 	BUG_ON(!(q->flags & SIGQUEUE_PREALLOC));
 
@@ -1519,6 +1519,7 @@ int send_sigqueue(struct sigqueue *q, struct task_struct *t, int group)
 		goto ret;
 
 	ret = 1; /* the signal is ignored */
+	result = TRACE_SIGNAL_IGNORED;
 	if (!prepare_signal(sig, t, 0))
 		goto out;
 
@@ -1530,6 +1531,7 @@ int send_sigqueue(struct sigqueue *q, struct task_struct *t, int group)
 		 */
 		BUG_ON(q->info.si_code != SI_TIMER);
 		q->info.si_overrun++;
+		result = TRACE_SIGNAL_ALREADY_PENDING;
 		goto out;
 	}
 	q->info.si_overrun = 0;
@@ -1539,7 +1541,9 @@ int send_sigqueue(struct sigqueue *q, struct task_struct *t, int group)
 	list_add_tail(&q->list, &pending->list);
 	sigaddset(&pending->signal, sig);
 	complete_signal(sig, t, group);
+	result = TRACE_SIGNAL_DELIVERED;
 out:
+	trace_signal_generate(sig, &q->info, t, group, result);
 	unlock_task_sighand(t, &flags);
 ret:
 	return ret;
