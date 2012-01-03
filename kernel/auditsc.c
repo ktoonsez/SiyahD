755fd77f120e242ac3ecdbe700bf75c5f651a298
Eric Paris <eparis@redhat.com>
2012-01-03 14:23:07 -0500

    audit: inline checks for not needing to collect aux records
    A number of audit hooks make function calls before they determine that
    auxilary records do not need to be collected.  Do those checks as static
    inlines since the most common case is going to be that records are not
    needed and we can skip the function call overhead.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>
    (cherry picked from commit 07c49417877f8658a6aa0ad9b4e21e4fd4df11b6)

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -2301,13 +2301,10 @@ void __audit_ipc_set_perm(unsigned long qbytes, uid_t uid, gid_t gid, umode_t mo
 	context->ipc.has_perm = 1;
 }
 
-int audit_bprm(struct linux_binprm *bprm)
+int __audit_bprm(struct linux_binprm *bprm)
 {
 	struct audit_aux_data_execve *ax;
 	struct audit_context *context = current->audit_context;
-
-	if (likely(!audit_enabled || !context || context->dummy))
-		return 0;
 
 	ax = kmalloc(sizeof(*ax), GFP_KERNEL);
 	if (!ax)
@@ -2329,12 +2326,9 @@ int audit_bprm(struct linux_binprm *bprm)
  * @args: args array
  *
  */
-void audit_socketcall(int nargs, unsigned long *args)
+void __audit_socketcall(int nargs, unsigned long *args)
 {
 	struct audit_context *context = current->audit_context;
-
-	if (likely(!context || context->dummy))
-		return;
 
 	context->type = AUDIT_SOCKETCALL;
 	context->socketcall.nargs = nargs;
@@ -2361,12 +2355,9 @@ void __audit_fd_pair(int fd1, int fd2)
  *
  * Returns 0 for success or NULL context or < 0 on error.
  */
-int audit_sockaddr(int len, void *a)
+int __audit_sockaddr(int len, void *a)
 {
 	struct audit_context *context = current->audit_context;
-
-	if (likely(!context || context->dummy))
-		return 0;
 
 	if (!context->sockaddr) {
 		void *p = kmalloc(sizeof(struct sockaddr_storage), GFP_KERNEL);
