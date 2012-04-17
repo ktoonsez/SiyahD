ce5d578d93feefef0fa559693a73b4fcc2fdb325
Dorimanx <yuri@bynet.co.il>
2012-04-17 14:53:06 +0300

    Revert "Ported myfluxi / xxICSKernel CPU levels and voltage!"
    This reverts commit 1a6ab43e5df834dac4d8a6b5538f983a6a166efa.

--- arch/arm/mach-exynos/include/mach/cpufreq.h
+++ arch/arm/mach-exynos/include/mach/cpufreq.h
@@ -17,9 +17,9 @@
 enum cpufreq_level_index {
 	L0, L1, L2, L3, L4,
 	L5, L6, L7, L8, L9,
-        L10, L11, L12, L13, L14,
-        L15, L16, L17, L18, L19,
-        L20,
+	L10, L11, L12, L13, L14,
+	L15, L16, L17, L18, L19,
+	L20,
 };
 
 enum busfreq_level_request {
@@ -68,6 +68,8 @@ struct exynos_dvfs_info {
 	unsigned int	pm_lock_idx;
 	unsigned int	max_support_idx;
 	unsigned int	min_support_idx;
+	unsigned int	max_current_idx;
+	unsigned int	min_current_idx;
 	struct clk	*cpu_clk;
 	unsigned int	*volt_table;
 	struct cpufreq_frequency_table	*freq_table;
