42bb85e8ed3eee54da9b318225c417e5c2d83271
Dorimanx <yuri@bynet.co.il>
2012-04-24 16:35:20 +0300

    Revert to default int min_free_order_shift = 1

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -190,7 +190,7 @@ static char * const zone_names[MAX_NR_ZONES] = {
 };
 
 int min_free_kbytes = 5120;
-int min_free_order_shift = 4;
+int min_free_order_shift = 1;
 
 static unsigned long __meminitdata nr_kernel_pages;
 static unsigned long __meminitdata nr_all_pages;
