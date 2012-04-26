46b465db3c358bcbfa134ac68315152caf872057
Dorimanx <yuri@bynet.co.il>
2012-04-26 16:48:43 +0300

    Add FAST_NO_HZ, update version + buildscript update.

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,6 +3,8 @@ export KERNELDIR=`readlink -f .`
 export INITRAMFS_SOURCE=`readlink -f $KERNELDIR/../initramfs3`
 export PARENT_DIR=`readlink -f ..`
 export USE_SEC_FIPS_MODE=true
+NAMBEROFCPUS=`grep 'processor' /proc/cpuinfo | wc -l`
+
 
 if [ "${1}" != "" ];then
   export KERNELDIR=`readlink -f ${1}`
@@ -55,7 +57,7 @@ export ARCH=arm
 export CROSS_COMPILE=/media/Source-Code/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 
 cd $KERNELDIR/
-nice -n 10 make -j8 modules || exit 1
+nice -n 10 make -j$NAMBEROFCPUS modules || exit 1
 
 #Copy initramfs files to tmp directory
 cp -ax $INITRAMFS_SOURCE $INITRAMFS_TMP
@@ -74,7 +76,7 @@ mkdir -p $INITRAMFS/lib/modules
 find -name '*.ko' -exec cp -av {} $INITRAMFS_TMP/lib/modules/ \;
 ${CROSS_COMPILE}strip --strip-debug $INITRAMFS_TMP/lib/modules/*.ko
 chmod 755 $INITRAMFS_TMP/lib/modules/*
-nice -n 10 make -j8 zImage CONFIG_INITRAMFS_SOURCE="$INITRAMFS_TMP" || exit 1
+nice -n 10 make -j$NAMBEROFCPUS zImage CONFIG_INITRAMFS_SOURCE="$INITRAMFS_TMP" || exit 1
 
 if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
 $KERNELDIR/mkshbootimg.py $KERNELDIR/zImage $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/payload.tar $KERNELDIR/recovery.tar.xz
