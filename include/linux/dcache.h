8aa030019b23006642318ccd6829c9c5e8b8b09e
Dorimanx <yuri@bynet.co.il>
2012-06-04 18:39:02 +0300

    vfs: trivial __d_lookup_rcu() cleanups
    These don't change any semantics, but they clean up the code a bit and
    mark some arguments appropriately 'const'.
    
    They came up as I was doing the word-at-a-time dcache name accessor
    code, and cleaning this up now allows me to send out a smaller relevant
    interesting patch for the experimental stuff.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    commit 8966be90304b394fd6a2c5af7b6b3abe2df3889c 1 parent 2273d5ccb8
    torvalds authored 3 months ago

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -87,14 +87,7 @@ static inline unsigned long end_name_hash(unsigned long hash)
 }
 
 /* Compute the hash for a name string. */
-static inline unsigned int
-full_name_hash(const unsigned char *name, unsigned int len)
-{
-	unsigned long hash = init_name_hash();
-	while (len--)
-		hash = partial_name_hash(*name++, hash);
-	return end_name_hash(hash);
-}
+extern unsigned int full_name_hash(const unsigned char *, unsigned int);
 
 /*
  * Try to keep struct dentry aligned on 64 byte cachelines (this will
@@ -306,7 +299,8 @@ extern struct dentry *d_ancestor(struct dentry *, struct dentry *);
 extern struct dentry *d_lookup(struct dentry *, struct qstr *);
 extern struct dentry *d_hash_and_lookup(struct dentry *, struct qstr *);
 extern struct dentry *__d_lookup(struct dentry *, struct qstr *);
-extern struct dentry *__d_lookup_rcu(struct dentry *parent, struct qstr *name,
+extern struct dentry *__d_lookup_rcu(const struct dentry *parent,
+				const struct qstr *name,
 				unsigned *seq, struct inode **inode);
 
 /**
