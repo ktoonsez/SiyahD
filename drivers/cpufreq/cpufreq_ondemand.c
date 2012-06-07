7fdf124b350c04e9c421560111cd48df4188a04b
Dorimanx <yuri@bynet.co.il>
2012-06-07 13:23:38 +0300

    kernel: power: prevent DVFS_LOCK_ID_USER lock for min and max frequency levels
    + GOVS tune and settings changes.
    
    commit a094ca788af2286bbaaaf5e604d775271b884c77 1 parent 9fbbf82972
    gokhanmoral authored 16 hours ago

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -35,24 +35,24 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_DOWN_DIFFERENTIAL         (10)
+#define DEF_FREQUENCY_DOWN_DIFFERENTIAL         (25)
 #define DEF_FREQUENCY_UP_THRESHOLD              (80)
 #define DEF_SAMPLING_DOWN_FACTOR                (5)
 #define MAX_SAMPLING_DOWN_FACTOR                (100000)
 #define MICRO_FREQUENCY_DOWN_DIFFERENTIAL       (1)
-#define MICRO_FREQUENCY_UP_THRESHOLD            (50)
+#define MICRO_FREQUENCY_UP_THRESHOLD            (60)
 #define MICRO_FREQUENCY_MIN_SAMPLE_RATE         (10000)
-#define MIN_FREQUENCY_UP_THRESHOLD              (11)
+#define MIN_FREQUENCY_UP_THRESHOLD              (10)
 #define MAX_FREQUENCY_UP_THRESHOLD              (100)
-#define FREQ_STEP                               (20)
-#define UP_THRESHOLD_AT_MIN_FREQ                (60)
+#define FREQ_STEP                               (40)
+#define UP_THRESHOLD_AT_MIN_FREQ                (40)
 #define FREQ_FOR_RESPONSIVENESS                 (600000)
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
 /* FIX ME! what is set here, will be on wake state also! */
-#define FREQ_STEP_SUSPEND                       (20)
+#define FREQ_STEP_SUSPEND                       (40)
 #define SAMPLING_FACTOR_SUSPEND                 (5)
-#define DEF_FREQUENCY_UP_THRESHOLD_SUSPEND      (50)
+#define DEF_FREQUENCY_UP_THRESHOLD_SUSPEND      (60)
 #endif
 
 /*
