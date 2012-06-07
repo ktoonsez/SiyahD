f3da41a1facb9a10ce67ebc7824d1765c65d73a5
Dorimanx <yuri@bynet.co.il>
2012-06-07 22:05:29 +0300

    Finished busfreq CODE. now all ok.

--- arch/arm/mach-exynos/busfreq.c
+++ arch/arm/mach-exynos/busfreq.c
@@ -127,9 +127,12 @@ static unsigned int clkdiv_dmc0[LV_END][8] = {
 
 	/* DMC L2: 133MHz */
 	{ 5, 2, 1, 5, 1, 1, 3, 1 },
+
+	/* DMC L3: 100MHz */
+	{ 5, 2, 1, 5, 1, 1, 3, 1 },
 };
 
-static unsigned int clkdiv_top[LV_END][5] = {
+static unsigned int clkdiv_top[LV_END][6] = {
 	/*
 	 * Clock divider value for following
 	 * { DIVACLK200, DIVACLK100, DIVACLK160, DIVACLK133, DIVONENAND }
@@ -142,6 +145,9 @@ static unsigned int clkdiv_top[LV_END][5] = {
 	{ 4, 7, 5, 6, 1 },
 
 	/* ACLK200 L2: 133MHz */
+	{ 5, 7, 7, 7, 1 },
+
+	/* ACLK200 L3: 100MHz */
 	{ 5, 7, 7, 7, 1 },
 };
 
@@ -184,7 +190,6 @@ static unsigned int clkdiv_ip_bus[LV_END][3] = {
 	/* L3: MFC 200MHz G2D 100MHz FIMC 100MHz */
 	/* { 5, 5, 7 }, */
 	{ 3, 5, 7 },
-
 };
 
 static void exynos4_set_busfreq(unsigned int div_index)
@@ -779,7 +784,7 @@ static int __init busfreq_mon_init(void)
 			(clkdiv_top[i][1] << EXYNOS4_CLKDIV_TOP_ACLK100_SHIFT) |
 			(clkdiv_top[i][2] << EXYNOS4_CLKDIV_TOP_ACLK160_SHIFT) |
 			(clkdiv_top[i][3] << EXYNOS4_CLKDIV_TOP_ACLK133_SHIFT) |
-			(clkdiv_top[i][4] << EXYNOS4_CLKDIV_TOP_ACLK100_SHIFT) |
+			(clkdiv_top[i][4] << EXYNOS4_CLKDIV_TOP_ACLK100_SHIFT) | 
 			(clkdiv_top[i][5] << EXYNOS4_CLKDIV_TOP_ONENAND_SHIFT));
 
 		exynos4_busfreq_table[i].clk_topdiv = tmp;
