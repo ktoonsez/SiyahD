dfdb649d3a2e6ca4e2d2c1cc2da39e56a391bf07
Lars Moelleken <voku@voku-online.de>
2012-06-09 16:32:26 +0200

    + revert changes ... -> http://git.kernel.org/?p=linux/kernel/git/maz/arm-platforms.git;a=commitdiff;h=1b76b02f15c70d5f392ee2e231fbd20a26063a77#patch1

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -98,7 +98,7 @@ EXPORT_SYMBOL(node_states);
 
 unsigned long totalram_pages __read_mostly;
 unsigned long totalreserve_pages __read_mostly;
-int percpu_pagelist_fraction = 8;
+int percpu_pagelist_fraction;
 gfp_t gfp_allowed_mask __read_mostly = GFP_BOOT_MASK;
 
 #ifdef CONFIG_PM_SLEEP
@@ -5360,7 +5360,7 @@ int percpu_pagelist_fraction_sysctl_handler(ctl_table *table, int write,
 	int ret;
 
 	ret = proc_dointvec_minmax(table, write, buffer, length, ppos);
-	if (!write || (ret < 0))
+	if (!write || (ret == -EINVAL))
 		return ret;
 	for_each_populated_zone(zone) {
 		for_each_possible_cpu(cpu) {
