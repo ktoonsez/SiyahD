212e3f6a45c67b8492bcdc572236611f5b80c903
Dorimanx <yuri@bynet.co.il>
2012-06-10 18:49:34 +0300

    Tuned busfreq THRESHOLD_DEFAULT

--- arch/arm/mach-exynos/busfreq.c
+++ arch/arm/mach-exynos/busfreq.c
@@ -45,8 +45,8 @@
 
 #define MAX_LOAD		100
 #define DIVIDING_FACTOR		10000
-#define UP_THRESHOLD_DEFAULT	23
-#define DOWN_THRESHOLD_DEFAULT	23
+#define UP_THRESHOLD_DEFAULT	30
+#define DOWN_THRESHOLD_DEFAULT	25
 
 static unsigned up_threshold;
 static unsigned down_threshold;
