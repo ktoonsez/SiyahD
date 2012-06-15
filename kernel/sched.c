db5b95163f34e035ed4d8aaf8797c043cee57b74
root <root@voku-VirtualBox.(none)>
2012-06-15 15:06:06 +0200

    + revert: sched, fair: Add some serialization to the sched_domain ... -> https://github.com/voku/siyahkernel3/commit/77435d1452ca5e44a65710f6593cf18d7e5c73fe

--- kernel/sched.c
+++ kernel/sched.c
@@ -7098,7 +7098,6 @@ build_overlap_sched_groups(struct sched_domain *sd, int cpu)
 
 		sg->sgp = *per_cpu_ptr(sdd->sgp, cpumask_first(sg_span));
 		atomic_inc(&sg->sgp->ref);
-		sg->balance_cpu = -1;
 
 		if (cpumask_test_cpu(cpu, sg_span))
 			groups = sg;
@@ -7174,7 +7173,6 @@ build_sched_groups(struct sched_domain *sd, int cpu)
 
 		cpumask_clear(sched_group_cpus(sg));
 		sg->sgp->power = 0;
-		sg->balance_cpu = -1;
 
 		for_each_cpu(j, span) {
 			if (get_group(j, sdd, NULL) != group)
