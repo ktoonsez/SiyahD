ba00b9ffd4564f80b23b7698d188b68bb47f1919
Dorimanx <yuri@bynet.co.il>
2012-06-24 23:01:48 +0300

    staging: android: lowmemorykiller: Fix task_struct leak ... -> http.
    .s://github.com/DerTeufel/samsung-kernel-aries/commit/1370f8c6e24a4182a45a7657a4b144cda2797ec4

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -36,7 +36,6 @@
 #include <linux/oom.h>
 #include <linux/sched.h>
 #include <linux/rcupdate.h>
-#include <linux/profile.h>
 #include <linux/notifier.h>
 #include <linux/memory.h>
 #include <linux/memory_hotplug.h>
