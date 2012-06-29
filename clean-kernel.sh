43cdd961019c4f6fcc9f3c98506978a5f8bb5b71
Dorimanx <yuri@bynet.co.il>
2012-06-29 22:04:09 +0300

    + testing2: CPUIdle Backport from 3.2 kernel + update: clean-kernel.sh + update: updater-script + revert changes: max8997-muic.c

--- clean-kernel.sh
+++ clean-kernel.sh
@@ -1,3 +1,4 @@
-cp .config .config.bkp
-make ARCH=arm CROSS_COMPILE=/media/Source-Code/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- mrproper
-cp .config.bkp .config
+cp .config .config.bkp;
+make ARCH=arm CROSS_COMPILE=linaro-12-05-android-toolchain/bin/arm-eabi- mrproper;
+cp .config.bkp .config;
+make clean;
