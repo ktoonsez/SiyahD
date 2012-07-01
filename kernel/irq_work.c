e1956104fcd0829e2f7318ed0e90ff37546241b0
Lars Moelleken <voku@voku-online.de>
2012-07-01 23:07:22 +0200

    + sched: Convert to struct llist -> https://github.com/DerTeufel/samsung-kernel-aries/commit/75fd76f9c723a6d7d7c9e63fb340b672050e8c75

--- kernel/irq_work.c
+++ kernel/irq_work.c
@@ -23,14 +23,7 @@
 #define IRQ_WORK_BUSY		2UL
 #define IRQ_WORK_FLAGS		3UL
 
-#define LIST_NONEMPTY_BIT	0
-
-struct irq_work_list {
-	unsigned long flags;
-	struct llist_head llist;
-};
-
-static DEFINE_PER_CPU(struct irq_work_list, irq_work_lists);
+static DEFINE_PER_CPU(struct llist_head, irq_work_list);
 
 /*
  * Claim the entry so that no one else will poke at it.
@@ -39,16 +32,18 @@ static bool irq_work_claim(struct irq_work *work)
 {
 	unsigned long flags, nflags;
 
-	do {
+	for (;;) {
 		flags = work->flags;
 		if (flags & IRQ_WORK_PENDING)
 			return false;
 		nflags = flags | IRQ_WORK_FLAGS;
-	} while (cmpxchg(&work->flags, flags, nflags) != flags);
+		if (cmpxchg(&work->flags, flags, nflags) == flags)
+			break;
+		cpu_relax();
+	}
 
 	return true;
 }
-
 
 void __weak arch_irq_work_raise(void)
 {
@@ -62,17 +57,16 @@ void __weak arch_irq_work_raise(void)
  */
 static void __irq_work_queue(struct irq_work *work)
 {
-	struct irq_work_list *irq_work_list;
+	bool empty;
 
-	struct irq_work_list *irq_work_list;
+	preempt_disable();
 
-	llist_add(&work->llnode, &irq_work_list->llist);
-
+	empty = llist_add(&work->llnode, &__get_cpu_var(irq_work_list));
 	/* The list was empty, raise self-interrupt to start processing. */
-	if (!test_and_set_bit(LIST_NONEMPTY_BIT, &irq_work_list->flags))
+	if (empty)
 		arch_irq_work_raise();
 
-	put_cpu_var(irq_work_list);
+	preempt_enable();
 }
 
 /*
@@ -102,22 +96,21 @@ EXPORT_SYMBOL_GPL(irq_work_queue);
 void irq_work_run(void)
 {
 	struct irq_work *work;
-	struct irq_work_list *irq_work_list;
+	struct llist_head *this_list;
 	struct llist_node *llnode;
 
-	irq_work_list = &__get_cpu_var(irq_work_lists);
-	if (llist_empty(&irq_work_list->llist))
+	this_list = &__get_cpu_var(irq_work_list);
+	if (llist_empty(this_list))
 		return;
 
 	BUG_ON(!in_irq());
 	BUG_ON(!irqs_disabled());
 
-	clear_bit(LIST_NONEMPTY_BIT, &irq_work_list->flags);
-	llnode = llist_del_all(&irq_work_list->llist);
+	llnode = llist_del_all(this_list);
 	while (llnode != NULL) {
 		work = llist_entry(llnode, struct irq_work, llnode);
 
-		llnode = llnode->next;
+		llnode = llist_next(llnode);
 
 		/*
 		 * Clear the PENDING bit, after this point the @work
--- lib/llist.c
+++ lib/llist.c
@@ -3,11 +3,11 @@
  *
  * The basic atomic operation of this list is cmpxchg on long.  On
  * architectures that don't have NMI-safe cmpxchg implementation, the
- * list can NOT be used in NMI handler.  So code uses the list in NMI
- * handler should depend on CONFIG_ARCH_HAVE_NMI_SAFE_CMPXCHG.
+ * list can NOT be used in NMI handlers.  So code that uses the list in
+ * an NMI handler should depend on CONFIG_ARCH_HAVE_NMI_SAFE_CMPXCHG.
  *
- * Copyright 2010 Intel Corp.
- *   Author: Huang Ying <ying.huang <at> intel.com>
+ * Copyright 2010,2011 Intel Corp.
+ *   Author: Huang Ying <ying.huang@intel.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
@@ -30,44 +30,28 @@
 #include <asm/system.h>
 
 /**
- * llist_add - add a new entry
- * @new:	new entry to be added
- * @head:	the head for your lock-less list
- */
-void llist_add(struct llist_node *new, struct llist_head *head)
-{
-	struct llist_node *entry;
-
-#ifndef CONFIG_ARCH_HAVE_NMI_SAFE_CMPXCHG
-	BUG_ON(in_nmi());
-#endif
-
-	do {
-		entry = head->first;
-		new->next = entry;
-	} while (cmpxchg(&head->first, entry, new) != entry);
-}
-EXPORT_SYMBOL_GPL(llist_add);
-
-/**
  * llist_add_batch - add several linked entries in batch
  * @new_first:	first entry in batch to be added
  * @new_last:	last entry in batch to be added
  * @head:	the head for your lock-less list
+ *
+ * Return whether list is empty before adding.
  */
-void llist_add_batch(struct llist_node *new_first, struct llist_node *new_last,
+bool llist_add_batch(struct llist_node *new_first, struct llist_node *new_last,
 		     struct llist_head *head)
 {
-	struct llist_node *entry;
+	struct llist_node *entry, *old_entry;
 
-#ifndef CONFIG_ARCH_HAVE_NMI_SAFE_CMPXCHG
-	BUG_ON(in_nmi());
-#endif
-
-	do {
-		entry = head->first;
+	entry = head->first;
+	for (;;) {
+		old_entry = entry;
 		new_last->next = entry;
-	} while (cmpxchg(&head->first, entry, new_first) != entry);
+		entry = cmpxchg(&head->first, old_entry, new_first);
+		if (entry == old_entry)
+			break;
+	}
+
+	return old_entry == NULL;
 }
 EXPORT_SYMBOL_GPL(llist_add_batch);
 
@@ -75,45 +59,31 @@ EXPORT_SYMBOL_GPL(llist_add_batch);
  * llist_del_first - delete the first entry of lock-less list
  * @head:	the head for your lock-less list
  *
- * If list is empty, return NULL, otherwise, return the first entry deleted.
+ * If list is empty, return NULL, otherwise, return the first entry
+ * deleted, this is the newest added one.
  *
  * Only one llist_del_first user can be used simultaneously with
- * multiple llist_add users without lock. Because otherwise
- * llist_del_first, llist_add, llist_add sequence in another user may
- * change @head->first->next, but keep @head->first. If multiple
- * consumers are needed, please use llist_del_all.
+ * multiple llist_add users without lock.  Because otherwise
+ * llist_del_first, llist_add, llist_add (or llist_del_all, llist_add,
+ * llist_add) sequence in another user may change @head->first->next,
+ * but keep @head->first.  If multiple consumers are needed, please
+ * use llist_del_all or use lock between consumers.
  */
 struct llist_node *llist_del_first(struct llist_head *head)
 {
-	struct llist_node *entry;
+	struct llist_node *entry, *old_entry, *next;
 
-#ifndef CONFIG_ARCH_HAVE_NMI_SAFE_CMPXCHG
-	BUG_ON(in_nmi());
-#endif
-
-	do {
-		entry = head->first;
+	entry = head->first;
+	for (;;) {
 		if (entry == NULL)
 			return NULL;
-	} while (cmpxchg(&head->first, entry, entry->next) != entry);
+		old_entry = entry;
+		next = entry->next;
+		entry = cmpxchg(&head->first, old_entry, next);
+		if (entry == old_entry)
+			break;
+	}
 
 	return entry;
 }
 EXPORT_SYMBOL_GPL(llist_del_first);
-
-/**
- * llist_del_all - delete all entries from lock-less list
- * @head:	the head of lock-less list to delete all entries
- *
- * If list is empty, return NULL, otherwise, delete all entries and
- * return the pointer to the first entry.
- */
-struct llist_node *llist_del_all(struct llist_head *head)
-{
-#ifndef CONFIG_ARCH_HAVE_NMI_SAFE_CMPXCHG
-	BUG_ON(in_nmi());
-#endif
-
-	return xchg(&head->first, NULL);
-}
-EXPORT_SYMBOL_GPL(llist_del_all);
