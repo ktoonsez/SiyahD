9fa8a4507442d785b6d6bf8ba33350cbcaf79c09
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-07-11 18:44:18 +0300

    mali: make mali resume frequency setting optional

--- drivers/media/video/samsung/mali/platform/pegasus-m400/mali_platform.c
+++ drivers/media/video/samsung/mali/platform/pegasus-m400/mali_platform.c
@@ -488,6 +488,7 @@ static mali_bool deinit_mali_clock(void)
 
 	return MALI_TRUE;
 }
+extern int mali_resume_freq_enabled;
 static _mali_osk_errcode_t enable_mali_clocks(void)
 {
 	int err;
@@ -497,7 +498,8 @@ static _mali_osk_errcode_t enable_mali_clocks(void)
 #if MALI_PMM_RUNTIME_JOB_CONTROL_ON
 #if MALI_DVFS_ENABLED
 	// set clock rate
-	if (get_mali_dvfs_control_status() != 0 || mali_gpu_clk >= mali_runtime_resume.clk)
+	if (get_mali_dvfs_control_status() != 0 || mali_gpu_clk >= mali_runtime_resume.clk
+		|| !mali_resume_freq_enabled)
 		mali_clk_set_rate(mali_gpu_clk, GPU_MHZ);
 	else {
 		mali_regulator_set_voltage(mali_runtime_resume.vol, mali_runtime_resume.vol);
