391fa11267847173228814cc450caa05d9687a7c
Dorimanx <yuri@bynet.co.il>
2012-07-20 14:54:19 +0300

    net: Statically initialize init_net.dev_base_head + MORE FIXING!
    This change eliminates an initialization-order hazard most
    recently seen when netprio_cgroup is built into the kernel.
    
    With thanks to Eric Dumazet for catching a bug.
    
    Signed-off-by: Mark Rustad <mark.d.rustad@intel.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    ========
    Tuned CPU high voltage -25 device heated on 1.5Ghz and below 2 steps.
    ========
    Updated INFO for memcopy patch.

--- lib/memcopy.c
+++ lib/memcopy.c
@@ -13,7 +13,7 @@
  * 
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  *
  * The code is derived from the GNU C Library.
  * Copyright (C) 1991, 1992, 1993, 1997, 2004 Free Software Foundation, Inc.
