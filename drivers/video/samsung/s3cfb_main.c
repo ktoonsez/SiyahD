635b204e6439d10bc0a71b2826bcacc4d3b52465
Dorimanx <yuri@bynet.co.il>
2012-07-29 13:02:15 +0300

    Restored BOOT Progress bar + MDNIE force suspend.

--- drivers/video/samsung/s3cfb_main.c
+++ drivers/video/samsung/s3cfb_main.c
@@ -49,13 +49,16 @@
 #include <linux/suspend.h>
 #endif
 
-#if defined(CONFIG_MACH_U1_BD) && defined(CONFIG_TARGET_LOCALE_KOR)
+#if defined(CONFIG_MACH_U1_BD) && defined(CONFIG_TARGET_LOCALE_EUR)
 #include <mach/regs-clock.h>
 #include "boot_progressbar.h"
 #define DISPLAY_BOOT_PROGRESS
 #endif
 #include <mach/regs-pmu.h>
 #include <plat/regs-fb-s5p.h>
+
+bool s3cfb_mdnie_force_disable;
+bool s3cfb_mdnie_suspended;
 
 struct s3cfb_fimd_desc		*fbfimd;
 
@@ -222,6 +225,9 @@ static int s3cfb_sysfs_store_win_power(struct device *dev,
 	int id, to;
 	struct s3cfb_global *fbdev[1];
 	fbdev[0] = fbfimd->fbdev[0];
+
+	if (s3cfb_mdnie_suspended)
+		return len;
 
 	while (*p != '\0') {
 		if (!isspace(*p))
@@ -498,6 +504,9 @@ static int s3cfb_probe(struct platform_device *pdev)
 	if (ret < 0)
 		dev_err(fbdev[0]->dev, "failed to add sysfs entries\n");
 
+	s3cfb_mdnie_force_disable = false;
+	s3cfb_mdnie_suspended = false;
+
 #ifdef DISPLAY_BOOT_PROGRESS
 	if (!(readl(S5P_INFORM2)))
 		s3cfb_start_progress(fbdev[0]->fb[pdata->default_win]);
@@ -664,6 +673,8 @@ void s3cfb_early_suspend(struct early_suspend *h)
 	int i, ret;
 
 	printk(KERN_INFO "+%s\n", __func__);
+
+	s3cfb_mdnie_suspended = true;
 
 #ifdef CONFIG_FB_S5P_MIPI_DSIM
 #if defined(CONFIG_FB_S5P_S6E63M0)
@@ -839,6 +850,7 @@ void s3cfb_late_resume(struct early_suspend *h)
 	s6e8ax0_late_resume();
 #endif
 #endif
+	s3cfb_mdnie_suspended = false;
 
 	printk(KERN_INFO "-%s\n", __func__);
 
