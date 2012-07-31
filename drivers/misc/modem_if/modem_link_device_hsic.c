0ef1b5ffbba2b7e7bc4182cba5c00df5f0214d7e
Dorimanx <yuri@bynet.co.il>
2012-08-01 01:36:17 +0300

    Shut Modem logs after new modem update.

--- drivers/misc/modem_if/modem_link_device_hsic.c
+++ drivers/misc/modem_if/modem_link_device_hsic.c
@@ -194,7 +194,9 @@ static void usb_rx_retry_work(struct work_struct *work)
 	switch (pipe_data->format) {
 	case IF_USB_FMT_EP:
 		if (usb_ld->if_usb_is_main) {
+#if 0 //Shut the DMESG Logs
 			pr_urb("IPC-RX, retry", urb);
+#endif
 			iod_format = IPC_FMT;
 		} else {
 			iod_format = IPC_BOOT;
@@ -269,7 +271,9 @@ static void usb_rx_complete(struct urb *urb)
 		switch (pipe_data->format) {
 		case IF_USB_FMT_EP:
 			if (usb_ld->if_usb_is_main) {
+#if 0 //Shut the DMESG Logs
 				pr_urb("IPC-RX", urb);
+#endif
 				iod_format = IPC_FMT;
 			} else {
 				iod_format = IPC_BOOT;
@@ -478,9 +482,10 @@ static int _usb_tx_work(struct sk_buff *skb)
 	if (!pipe_data)
 		return -ENOENT;
 
+#if 0 //shut the dmesg log
 	if (iod->format == IPC_FMT && usb_ld->if_usb_is_main)
 		pr_skb("IPC-TX", skb);
-
+#endif
 	if (iod->format == IPC_RAW)
 		mif_debug("TX[RAW]\n");
 
