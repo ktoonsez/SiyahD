a1ee3a85622ba2276d49413d9152185770a1f6ce
Dorimanx <yuri@bynet.co.il>
2012-08-12 00:11:45 +0300

    Also mount with NODIRATIME by DEFAULT!

--- fs/namespace.c
+++ fs/namespace.c
@@ -2324,9 +2324,9 @@ long do_mount(char *dev_name, char *dir_name, char *type_page,
 		mnt_flags |= MNT_NODEV;
 	if (flags & MS_NOEXEC)
 		mnt_flags |= MNT_NOEXEC;
-	if (flags & MS_NOATIME)
+//	if (flags & MS_NOATIME)
 		mnt_flags |= MNT_NOATIME;
-	if (flags & MS_NODIRATIME)
+//	if (flags & MS_NODIRATIME)
 		mnt_flags |= MNT_NODIRATIME;
 	if (flags & MS_STRICTATIME)
 		mnt_flags &= ~(MNT_RELATIME | MNT_NOATIME);
