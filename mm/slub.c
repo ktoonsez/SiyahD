a46962c97a269ad81b11c982b282c58e21261374
Dorimanx <yuri@bynet.co.il>
2012-08-19 11:35:44 +0300

    slub: fix a possible memleak in __slab_alloc()
    Zhihua Che reported a possible memleak in slub allocator on
    CONFIG_PREEMPT=y builds.
    
    It is possible current thread migrates right before disabling irqs in
    __slab_alloc(). We must check again c->freelist, and perform a normal
    allocation instead of scratching c->freelist.
    
    Many thanks to Zhihua Che for spotting this bug, introduced in 2.6.39
    
    V2: Its also possible an IRQ freed one (or several) object(s) and
    populated c->freelist, so its not a CONFIG_PREEMPT only problem.
    
    Cc: <stable@vger.kernel.org>        [2.6.39+]
    Reported-by: Zhihua Che <zhihua.che@gmail.com>
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Acked-by: Christoph Lameter <cl@linux.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- mm/slub.c
+++ mm/slub.c
@@ -2192,7 +2192,7 @@ redo:
 	/* must check again c->freelist in case of cpu migration or IRQ */
 	object = c->freelist;
 	if (object)
-		goto update_freelist;
+		goto load_freelist;
 
 	stat(s, ALLOC_SLOWPATH);
 
