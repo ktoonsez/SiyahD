12bf3da757986b2e1bd8cae4aa5a3312254a10e1
Dorimanx <yuri@bynet.co.il>
2012-08-19 03:49:38 +0300

    Updated SQLB with all missing from original commit.

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -74,8 +74,6 @@ extern void do_trace_rcu_torture_read(char *rcutorturename,
 #define ULONG_CMP_GE(a, b)	(ULONG_MAX / 2 >= (a) - (b))
 #define ULONG_CMP_LT(a, b)	(ULONG_MAX / 2 < (a) - (b))
 
-/* Exported common interfaces */
-
 #ifdef CONFIG_PREEMPT_RCU
 
 /**
@@ -138,6 +136,8 @@ extern void call_rcu_bh(struct rcu_head *head,
  *  anything that disables preemption.
  *  These may be nested.
  */
+
+/* Exported common interfaces */
 extern void call_rcu_sched(struct rcu_head *head,
 			   void (*func)(struct rcu_head *rcu));
 
