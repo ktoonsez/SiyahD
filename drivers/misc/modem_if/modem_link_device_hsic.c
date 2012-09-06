fbc367411d084adf72149ffe4c3ae3951bdceff9
Dorimanx <yuri@bynet.co.il>
2012-09-07 01:09:18 +0300

    No nonger need to remove debug alerts, global MODEM debug is off!

--- drivers/misc/modem_if/modem_link_device_hsic.c
+++ drivers/misc/modem_if/modem_link_device_hsic.c
@@ -194,9 +194,7 @@ static void usb_rx_retry_work(struct work_struct *work)
 	switch (pipe_data->format) {
 	case IF_USB_FMT_EP:
 		if (usb_ld->if_usb_is_main) {
-#if 0
 			pr_urb("IPC-RX, retry", urb);
-#endif
 			iod_format = IPC_FMT;
 		} else {
 			iod_format = IPC_BOOT;
@@ -207,9 +205,7 @@ static void usb_rx_retry_work(struct work_struct *work)
 		break;
 	case IF_USB_RFS_EP:
 		iod_format = IPC_RFS;
-#if 0
 		pr_urb("RFS-RX, retry", urb);
-#endif
 		break;
 	case IF_USB_CMD_EP:
 		iod_format = IPC_CMD;
@@ -273,9 +269,7 @@ static void usb_rx_complete(struct urb *urb)
 		switch (pipe_data->format) {
 		case IF_USB_FMT_EP:
 			if (usb_ld->if_usb_is_main) {
-#if 0
 				pr_urb("IPC-RX", urb);
-#endif
 				iod_format = IPC_FMT;
 			} else {
 				iod_format = IPC_BOOT;
@@ -483,10 +477,8 @@ static int _usb_tx_work(struct sk_buff *skb)
 
 	if (!pipe_data)
 		return -ENOENT;
-#if 0
 	if (iod->format == IPC_FMT && usb_ld->if_usb_is_main)
 		pr_skb("IPC-TX", skb);
-#endif
 	if (iod->format == IPC_RAW)
 		mif_debug("TX[RAW]\n");
 
