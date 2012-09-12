502e6cf1d0b65be8bd489a109efe53053d3f6191
Dorimanx <yuri@bynet.co.il>
2012-09-12 18:57:00 +0300

    mmc: card: Skip secure erase on MoviNAND; causes unrecoverable corruption.
    For several MoviNAND eMMC parts, there are known issues with secure
    erase and secure trim.  For these specific MoviNAND devices, we skip
    these operations.
    
    Specifically, there is a bug in the eMMC firmware that causes
    unrecoverable corruption when the MMC is erased with MMC_CAP_ERASE
    enabled.
    
    References:
    
    http://forum.xda-developers.com/showthread.php?t=1644364
    https://plus.google.com/111398485184813224730/posts/21pTYfTsCkB#111398485184813224730/posts/21pTYfTsCkB
    
    Signed-off-by: Ian Chen <ian.cy.chen@samsung.com>
    Reviewed-by: Namjae Jeon <linkinjeon@gmail.com>
    Acked-by: Jaehoon Chung <jh80.chung@samsung.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Cc: stable <stable@vger.kernel.org> [3.0+]
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -207,6 +207,10 @@ struct mmc_card {
 #define MMC_QUIRK_INAND_CMD38	(1<<6)		/* iNAND devices have broken CMD38 */
 #define MMC_QUIRK_BLK_NO_CMD23	(1<<7)		/* Avoid CMD23 for regular multiblock */
 
+#define MMC_QUIRK_BROKEN_BYTE_MODE_512 (1<<8)	/* Avoid sending 512 bytes in */
+#define MMC_QUIRK_LONG_READ_TIME (1<<9)		/* Data read time > CSD says */
+#define MMC_QUIRK_SEC_ERASE_TRIM_BROKEN (1<<10)	/* Skip secure for erase/trim */
+						/* byte mode */
 	unsigned int    poweroff_notify_state;	/* eMMC4.5 notify feature */
 #define MMC_NO_POWER_NOTIFICATION	0
 #define MMC_POWERED_ON			1
