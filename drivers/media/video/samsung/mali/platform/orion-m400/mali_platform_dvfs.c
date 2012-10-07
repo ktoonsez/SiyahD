3a3f1c8bf2bc22d050a6d4909e08d9e5e832185b
Dorimanx <yuri@bynet.co.il>
2012-10-07 13:25:49 +0200

    Merged CM10 New MALI driver! with fixed mem leaks! Now i need to restore some of my stuff back. many good things lost!
    now where is my hammer! lets polish this :)

--- drivers/media/video/samsung/mali/platform/orion-m400/mali_platform_dvfs.c
+++ drivers/media/video/samsung/mali/platform/orion-m400/mali_platform_dvfs.c
@@ -36,7 +36,7 @@
 #define EXYNOS4_ASV_ENABLED
 #endif
 
-static int bMaliDvfsRun = 0;
+static int bMaliDvfsRun=0;
 
 static _mali_osk_atomic_t bottomlock_status;
 static int bottom_lock_step;
@@ -116,29 +116,29 @@ mali_dvfs_threshold_table mali_dvfs_threshold[MALI_DVFS_STEPS]={
 
 static unsigned int asv_3d_volt_5_table[ASV_5_LEVEL][MALI_DVFS_STEPS] = {
 	/* L4(108MHz), L3(160MHz), L2(266MHz), L1(330MHz) */
-	{ 950000, 1000000, 1100000, 1150000},	/* S */
-	{ 950000, 1000000, 1100000, 1150000},	/* A */
-	{ 900000,  950000, 1000000, 1100000},	/* B */
-	{ 900000,  950000, 1000000, 1050000},	/* C */
-	{ 900000,  950000,  950000, 1000000},	/* D */
+	{ 975000, 1000000, 1100000, 1150000},	/* S */
+	{ 975000, 1000000, 1100000, 1150000},	/* A */
+	{ 925000,  950000, 1000000, 1100000},	/* B */
+	{ 925000,  950000, 1000000, 1050000},	/* C */
+	{ 925000,  950000,  950000, 1000000},	/* D */
 };
 
 static unsigned int asv_3d_volt_8_table[ASV_8_LEVEL][MALI_DVFS_STEPS] = {
 	/* L4(108MHz), L3(160MHz), L2(266MHz)), L1(330MHz) */
-	{ 950000, 1000000, 1100000, 1150000},	/* SS */
-	{ 950000, 1000000, 1100000, 1150000},	/* A1 */
-	{ 950000, 1000000, 1100000, 1150000},	/* A2 */
-	{ 900000,  950000, 1000000, 1100000},	/* B1 */
-	{ 900000,  950000, 1000000, 1100000},	/* B2 */
-	{ 900000,  950000, 1000000, 1050000},	/* C1 */
-	{ 900000,  950000, 1000000, 1050000},	/* C2 */
-	{ 900000,  950000,  950000, 1000000},	/* D1 */
+	{ 975000, 1000000, 1100000, 1150000},	/* SS */
+	{ 975000, 1000000, 1100000, 1150000},	/* A1 */
+	{ 975000, 1000000, 1100000, 1150000},	/* A2 */
+	{ 925000,  950000, 1000000, 1100000},	/* B1 */
+	{ 925000,  950000, 1000000, 1100000},	/* B2 */
+	{ 925000,  950000, 1000000, 1050000},	/* C1 */
+	{ 925000,  950000, 1000000, 1050000},	/* C2 */
+	{ 925000,  950000,  950000, 1000000},	/* D1 */
 };
 #endif
 
 /*dvfs status*/
 mali_dvfs_currentstatus maliDvfsStatus;
-int mali_dvfs_control = 0;
+int mali_dvfs_control=0;
 
 u32 mali_dvfs_utilization = 255;
 
@@ -507,4 +507,3 @@ int mali_dvfs_bottom_lock_pop(void)
 
 	return _mali_osk_atomic_dec_return(&bottomlock_status);
 }
-
--- drivers/video/samsung/mdnie.c
+++ drivers/video/samsung/mdnie.c
@@ -419,7 +419,7 @@ static int mdnie_set_brightness(struct backlight_device *bd)
 
 	if ((mdnie->enable) && (mdnie->bd_enable)) {
 		ret = update_brightness(mdnie);
-		dev_dbg(&bd->dev, "brightness=%d\n", bd->props.brightness);
+		dev_info(&bd->dev, "brightness=%d\n", bd->props.brightness);
 		if (ret < 0)
 			return -EINVAL;
 	}
@@ -805,7 +805,7 @@ void mdnie_late_resume(struct early_suspend *h)
 		pd->power_on(NULL, 1);
 
 	if (mdnie->enable) {
-		dev_dbg(&mdnie->bd->dev, "brightness=%d\n", mdnie->bd->props.brightness);
+		dev_info(&mdnie->bd->dev, "brightness=%d\n", mdnie->bd->props.brightness);
 		update_brightness(mdnie);
 	}
 
