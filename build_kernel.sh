f324bed0046475a9ec4e8b02d24f3c5ad3390fef
Dorimanx <yuri@bynet.co.il>
2012-10-16 01:19:41 +0200

    Added Voku's auto kernel copy to sdcard, and improved kernel build speed. My server has 16CPU so i can work with 32 treads with no problem. kernel build time is much faster.

--- build_kernel.sh
+++ build_kernel.sh
@@ -42,7 +42,13 @@ fi;
 # build script
 export USER=`whoami`
 
-NAMBEROFCPUS=`grep 'processor' /proc/cpuinfo | wc -l`
+HOST_CHECK=`uname -n`
+if [ $HOST_CHECK == dorimanx-virtual-machine ]; then
+	NAMBEROFCPUS=32
+	echo "Dori power detected!"
+else
+	NAMBEROFCPUS=`grep 'processor' /proc/cpuinfo | wc -l`
+fi;
 INITRAMFS_TMP="/tmp/initramfs-source"
 
 if [ "${1}" != "" ];
@@ -150,6 +156,10 @@ if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
 	cp $KERNELDIR/zImage /$KERNELDIR/READY-JB/boot/
 	cd $KERNELDIR/READY-JB/
 	zip -r Kernel_Dorimanx-$GETVER-JB-MALI`date +"-%H-%M--%d-%m-12-SGII-PWR-CORE"`.zip .
+	STATUS=`adb get-state`;
+	if [ "$STATUS" == "device" ]; then
+		adb push $KERNELDIR/READY-JB/Kernel_Dorimanx*JB-MALI*.zip /sdcard/;
+	fi;
 else
 	echo "Kernel STUCK in BUILD! no zImage exist"
 fi;
