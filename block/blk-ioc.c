5022b004873ac024bd8da7529295abe57631e7d0
Dorimanx <yuri@bynet.co.il>
2012-10-17 13:56:34 +0200

    block: misc ioc cleanups
    * int return from put_io_context() wasn't used by anybody.  Make it
      return void like other put functions and docbook-fy the function
      comment.
    
    * Reorder dummy declarations for !CONFIG_BLOCK case a bit.
    
    * Make alloc_ioc_context() use __GFP_ZERO allocation, take init out of
      if block and drop 0'ing.
    
    * Docbook-fy current_io_context() comment.
    
    This patch doesn't introduce any functional change.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    modified by faux123 and Dorimanx

--- block/blk-ioc.c
+++ block/blk-ioc.c
@@ -27,28 +27,29 @@ static void hlist_sched_dtor(struct io_context *ioc, struct hlist_head *list)
 	}
 }
 
-/*
- * IO Context helper functions. put_io_context() returns 1 if there are no
- * more users of this io context, 0 otherwise.
+/**
+ * put_io_context - put a reference of io_context
+ * @ioc: io_context to put
+ *
+ * Decrement reference count of @ioc and release it if the count reaches
+ * zero.
  */
-int put_io_context(struct io_context *ioc)
+void put_io_context(struct io_context *ioc)
 {
 	if (ioc == NULL)
-		return 1;
+		return;
 
-	BUG_ON(atomic_long_read(&ioc->refcount) == 0);
+	BUG_ON(atomic_long_read(&ioc->refcount) <= 0);
 
-	if (atomic_long_dec_and_test(&ioc->refcount)) {
+	if (!atomic_long_dec_and_test(&ioc->refcount))
+		return;
+
 		rcu_read_lock();
-
 		hlist_sched_dtor(ioc, &ioc->cic_list);
 		hlist_sched_dtor(ioc, &ioc->bfq_cic_list);
 		rcu_read_unlock();
 
 		kmem_cache_free(iocontext_cachep, ioc);
-		return 1;
-	}
-	return 0;
 }
 EXPORT_SYMBOL(put_io_context);
 
